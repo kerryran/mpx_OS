d5afd69dbabb8c48547f3723cbbb7bf7838d62ef
Kerrigan Ranshaw <kmr00004@mail.wvu.edu>
2023-08-21 18:53:24 -0400

    attempt at comhand

--- user/comhand.c
+++ user/comhand.c
@@ -1,9 +1,20 @@
+#include <../include/sys_req.h>
+#include <stdbool.h>
+#include <mpx/serial.h>
+
 void comhand(void) {
     while (true){
         char buf[100] = { 0 };
-        int nread = sys_req(READ, COM1, buf, sizeof(buf));
+        int buf_size = sizeof(buf);
+
+        serial_out(COM1, "whaty\0", 5);
+        //Menu Prompt
+        sys_req(WRITE, COM1, "buf", "hello I'm a menu");
+
+        int nread = sys_req(READ, COM1, buf, buf_size);
         //Echo the input to terminal
         sys_req(WRITE, COM1, buf, nread);
         //Command handling
+        
     }
 }
