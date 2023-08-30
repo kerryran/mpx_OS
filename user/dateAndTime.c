e50e47094c9624262e6fcd0af7190535d304ea85
med00005 <med00005@mix.wvu.edu>
2023-08-30 09:57:14 -0400

    smaller updates but shifting is not working

--- user/dateAndTime.c
+++ user/dateAndTime.c
@@ -16,23 +16,31 @@ void rtc_write(uint8_t reg, uint8_t value)
     sti(); // enable interrupts
 }
 
-void get_time()
+char* get_time()
 {
    
 
     outb(0x70, 0x00);
     unsigned char seconds = inb(0x00);
     //convert the shift??
+    int ones_sec = (seconds & 00001111);
 
     outb(0x70, 0x02);
     unsigned char minutes = inb(0x02);
     //convert the shift??
+    int ones_min = (minutes & 00001111);
 
     outb(0x70,0x04);
     unsigned char hours = inb(0x04);
     //convert the shift??
+    int ones_hr = (hours & 00001111);
 
-    
+    //delete the following later
+    puts((char*)ones_sec);
+    puts((char*)ones_min);
+    puts((char*)ones_hr);
+    char* time = "time";
+    return time;
     
 }
 
@@ -41,14 +49,22 @@ void get_date()
     outb(0x70, 0x07);
     unsigned char day = inb(0x07);
     //convert the shift
+    int ones_day = (day & 00001111);
 
     outb(0x70,0x08);
     unsigned char month = inb(0x08);
     //convert the shift
-    
+    int ones_month = (month & 00001111);
+
     outb(0x70,0x09);
     unsigned char year = inb(0x09);
     //convert the shift
+    int ones_year = (year & 00001111);
+
+    //delete the following later
+    puts((char*)ones_day);
+    puts((char*)ones_month);
+    puts((char*)ones_year);
     
 }
 
