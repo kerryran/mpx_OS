f4f3d92db6a8b7430d68024db8c852b4c13b14e0
anh0034 <hayandrew2@gmail.com>
2023-09-11 13:50:35 -0400

    actually fixed backspace, really this time charcnt

--- kernel/serial.c
+++ kernel/serial.c
@@ -112,11 +112,17 @@ int serial_poll(device dev, char *buffer, size_t len)
 				{
 					// Remove last character from the buffer
 					// Output backspace and a space to clear the previous character
+					
 					buffer_count--;
 					outb(dev, '\b');
 					outb(dev, ' ');
 					outb(dev, '\b');
+					char_count--;
 					buffer[buffer_count] = 0;
+				}
+				else
+				{
+
 				}
 			}
 				else if (c == '\x5B') {
