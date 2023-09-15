b577cec80d5b535afe9a7a9b294587c50129d7dd
sophiaadavid <89485763+sophiaadavid@users.noreply.github.com>
2023-09-15 14:12:20 -0400

    comments in pcbuser

---
+++ include/pcbuser.h
@@ -0,0 +1,23 @@
+//include whatever needed files.
+
+//create
+
+//delete
+
+//block
+
+//unblock
+
+//suspend
+
+//resume
+
+//set priority
+
+//show PCB
+
+//show ready
+
+//show blocked
+
+//show all
