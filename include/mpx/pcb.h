c3c7492bd66cd4e557c392a6c817bd75976f73f3
Kerrigan Ranshaw <kmr00004@mail.wvu.edu>
2023-09-20 13:26:44 -0400

    starting pcb

---
+++ include/mpx/pcb.h
@@ -0,0 +1,5 @@
+struct pcb *pcb_find(const char *);
+
+void pcb_insert(struct pcb *);
+
+int pcb_remove(struct pcb *);
--- kernel/pcb.c
+++ kernel/pcb.c
@@ -1,1 +1,16 @@
-//PCB Functions
+// PCB Functions
+
+// Find
+struct pcb *pcb_find(const char *)
+{
+}
+
+// Insert
+void pcb_insert(struct pcb *)
+{
+}
+
+// Remove
+int pcb_remove(struct pcb *)
+{
+}
