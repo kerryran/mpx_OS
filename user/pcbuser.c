5acee9ec70b46d02bef03b552b65f16667fd1c98
sophiaadavid <89485763+sophiaadavid@users.noreply.github.com>
2023-09-20 09:05:38 -0400

    started some user functions

---
+++ user/pcbuser.c
@@ -0,0 +1,32 @@
+//include whatever needed files.
+
+struct pcb* pcb_create(char* name, char* class, char* priority){
+
+    
+
+    return 0;
+}
+
+//delete
+void pcb_delete(char* name){
+
+    return 0;
+}
+
+//block
+
+//unblock
+
+//suspend
+
+//resume
+
+//set priority
+
+//show PCB
+
+//show ready
+
+//show blocked
+
+//show all
