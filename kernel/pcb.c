09a1bb309126a7b8ca6010602b5d2fb600fc3e0b
makay <med00005@mix.wvu.edu>
2023-09-22 12:20:01 -0400

    updates to pcb.c with initilization

--- kernel/pcb.c
+++ kernel/pcb.c
@@ -1,24 +1,47 @@
+#include <memory.h>
+#include <stdio.h>
+
 // Number of characters a pcb name can be
 int MAX_NAME_LEN = 8;
 
+typedef enum class{
+    SYSTEM = 0,
+    USER = 1
+}class;
+
+typedef enum state{
+    READY = 0,
+    BLOCKED = 1,
+    SUSPENDED_READY = 2,
+    SUSPENDED_BLOCKED = 3
+}state;
+
 // The struct we need
-struct pcb
+typedef struct pcb
 {
     char *name_ptr;
     char name_arr[MAX_NAME_LEN];
     int class;    // 0 for system, 1 for user
     int priority; // 0 = highest, 9 = lowest
-}
+    char stack[1024];
+}pcb;
 
 // idk what this is but we need it
-struct pcb *queue_head;
+pcb *queue_head;
 
 // Allocate memory for a new PCB, including the stack and in
 // and basic initialization
-struct pcb *pcb_allocate(void)
+pcb *pcb_allocate(void)
 {
     //need to check if this acctually allocates, if not return NULL
     struct pcb *some_pcb = sys_alloc_mem(sizeof(*some_pcb));
+
+   some_pcb->name_ptr = sys_alloc_mem(some_pcb->name_ptr);
+
+    if(some_pcb == NULL || some_pcb->name_ptr == NULL){
+        puts("Unable to allocate memory");
+        return NULL;
+    }
 
     return *some_pcb;
 
@@ -27,11 +50,34 @@ struct pcb *pcb_allocate(void)
 // Free all memory associated with a PCB, including the stack
 int pcb_free(struct pcb *pcb)
 {
+    int free =  sys_free_mem(pcb->name_ptr);
+
+    int free_pcb = sys_free_mem(pcb);
+
+    if(free == 0 && free_pcb == 0){
+        //successful free
+        return 0;
+    }
+    else{
+        //unsuccessful free
+        return -1;
+    }
 }
 
 // Allocate (via allocate_pcb()) a new PCB and initialize it with data provided, sets the state to Ready, Not-suspeneded
 struct pcb *pcb_setup(const char *name, int class, int priority)
 {
+    
+    struct pcb *new_pcb = allocate_pcb();
+
+    new_pcb->name_ptr = name;
+
+    new_pcb->class = class;
+
+    new_pcb->priority = priority; 
+
+    //somehow set the state
+
 }
 
 // Search all process queues for a process with the provided name
