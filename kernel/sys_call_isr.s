6272b8f2a47f1daeb458b3d7ffb0977fd4812301
anh0034 <hayandrew2@gmail.com>
2023-10-13 14:45:27 -0400

    asm

--- kernel/sys_call_isr.s
+++ kernel/sys_call_isr.s
@@ -1,8 +1,14 @@
 bits 32
+
 global sys_call_isr
 
 ;;; System call interrupt handler. To be implemented in Module R3.
-extern sys_call			; The C function that sys_call_isr will call
+extern sys_call			; allows asm to view the c function
 sys_call_isr:
-	mov eax, -1		;; DELETE THIS LINE WHEN YOU IMPLEMENT R3
+	pusha ;CHANGE THIS to push in opposide order of struct once struct is made
+	push ESP
+    call sys_call_isr
+	mov ESP, [EAX] ;set ESP to the value that EAX points to, or remove the [   ] if sys_call_isr returns a value not a pointer idk yet
+    popa ;CHANGE THIS to pop in order of struct once struct is made
 	iret
+	
