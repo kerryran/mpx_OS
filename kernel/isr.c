d3ffe15128c923519e619b634a49ebf68981a336
Kerrigan Ranshaw <kmr00004@mail.wvu.edu>
2023-10-13 14:44:07 -0400

    added isr.c and isr.h

---
+++ kernel/isr.c
@@ -0,0 +1,38 @@
+#include <../include/mpx/pcb.h>
+#include <../include/mpx/isr.h>
+
+// all registers for the context
+typedef struct context
+{
+    int CS;
+    int DS;
+    int ES;
+    int FS;
+    int GS;
+    int SS;
+    int EI;
+    int EFLAGS;
+    int EAX;
+    int EBX;
+    int ECX;
+    int EDX;
+    int ESI;
+    int EDI;
+    int EBP;
+    int ESP;
+
+} context;
+
+// Global PCB pointer for currently executing process
+pcb *pcb_exec = NULL;
+
+// Context pointer from first sys_call()
+static context *first_cont = NULL;
+
+struct context *sys_call(context *cont)
+{
+    if (first_cont == NULL)
+    {
+        first_cont = cont;
+    }
+}
