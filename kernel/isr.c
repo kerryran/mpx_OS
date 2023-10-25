7db3f4ab4e56f5d17c8fe8cffeaf76113c2e4b76
Kerrigan Ranshaw <kmr00004@mail.wvu.edu>
2023-10-25 15:48:01 -0400

    more code for r3

--- kernel/isr.c
+++ kernel/isr.c
@@ -4,9 +4,8 @@
 #include <stddef.h>
 
 pcb *pcb_exec = NULL;
-context *first_cont = NULL;
-
 pcb *next = NULL;
+context *first_cont = NULL;
 
 struct context *sys_call(struct context *cont)
 {
@@ -15,60 +14,69 @@ struct context *sys_call(struct context *cont)
 
     // Declare a context pointer to be returned
     pcb *next = NULL;
-    pcb *ready = ready_head
+    pcb *ready = ready_head;
 
     // Handle system call based on the operation code
     switch (operation_code)
     {
-        case EXIT:
-           if (first_cont == NULL)
-    {
-        first_cont = cont;
-    }
-            if (next_context == NULL) {
-                next_context = first_cont;
-            }
+        // If context is EXIT
+    case EXIT:
+        // Check if this is the first time sys_call has been called
+        if (first_cont == NULL)
+        {
+            // Save the context
+            first_cont = cont;
+        }
 
-            pcb_free(ready);
-            
-            cont->EAX = 0;
-            break;
+        pcb_free(ready);
 
-        case IDLE:
-           if (first_cont == NULL)
-             {
-             first_cont = cont;
-             }
-            // Handle IDLE case
-            if (ready == NULL){
-                return cont;
-            }
-
-            next = ready;
-            next->execute = 0;
-            next->dispatch = 4;
-            pcb_remove(ready);
-
-            if (ready != NULL) {
-                ready->execute = 0;
-                ready->dispatch = 4;
-                //ready-.stackptr = cont;
-                pcb_insert(ready)
-            }
-            ready = next;
-            //return next->stackpr;
-
-
-
-            cont->EAX = 0;
-            break;
-
-        default:
-            // Set the return value to -1 for unknown operation codes
-            cont->EAX = -1;
+        if (ready == NULL)
+        {
             return cont;
-    }
+        }
 
-    // Return context pointer
-    return next_context;
+        pcb_remove(ready);
+        next = ready;
+        next->execute = 0;  // Ready
+        next->dispatch = 4; // Suspended
+
+        // return (context) stackptr;
+
+        cont->EAX = 0;
+        break; // Remove this when the return is implemented
+    // If context is IDLE
+    case IDLE:
+        // Check if this is the first time sys_call has been called
+        if (first_cont == NULL)
+        {
+            // Save the context
+            first_cont = cont;
+        }
+        // Handle IDLE case
+        if (ready == NULL)
+        {
+            return cont;
+        }
+
+        next = ready;
+        next->execute = 0;  // Ready
+        next->dispatch = 4; // Suspended
+        pcb_remove(ready);
+
+        if (ready != NULL)
+        {
+            ready->execute = 0;
+            ready->dispatch = 4;
+            // ready-.stackptr = cont;
+            pcb_insert(ready);
+        }
+        ready = next;
+        // return next->stackpr;
+        break; // remove this when the return is implemented
+
+    default:
+        // Set the return value to -1 for unknown operation codes
+        cont->EAX = -1;
+        return cont;
+    }
 }
