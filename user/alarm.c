4593825c7e6356b6c632b9d11efbd1626d6ee4ae
med00005 <med00005@mix.wvu.edu>
2023-10-30 20:56:08 -0400

    new alarm

--- user/alarm.c
+++ user/alarm.c
@@ -13,53 +13,24 @@
 //#include <pcb.h>
 #include <pcbuser.h>
 
-char* alarm(char* time, char* message){
-        pcb* pcb = pcb_setup("alarm", 1, 1);
-        (context*) pcb->stack_ptr = sys_call(IDLE);
-        char* current_time = get_time();
-        if(strcmp(current_time, time)==0){
-                puts(message);
-                return message;
-                (context*) pcb->stack_ptr = sys_call(EXIT);
-        }
-        return "no alarm";
+typedef struct alarm{
+    char* time;
+    char* message;
+}alarm;
+
+
+void alarm(char* time, char* message){
+
+    alarm *new_alarm = sys_alloc_mem(sizeof(*new_alarm));
+
+    //Initialize time input
+    int seconds = 0;
+    int minutes = 0;
+    int hours = 0;
+
+    //Gather time input from user
+    
+
 }
 
-alarm_val *alarms;
-int exit;
 
-void add_alarm(alarm *alarm_to_add){
-    if (alarms == NULL){
-        alarms = ((sys_alloc_men(sizeof(*alarms))));
-        alarms->head = NULL;
-        alarms->tail = NULL;
-    }
-
-    alarm_val *temp = alarms;
-
-    if(alarm_to_add == NULL){
-        puts("The queue is empty")
-        return;
-    }
-
-    if(temp->tail != NULL){
-        temp->tail->next = alarm_to_add;
-        alarm_to_add->prev = temp->tail;
-        temp->tail = alarm_to_add;
-    }else{
-        temp->head = alarm_to_add;
-        temp->tail = alarm_to_add;
-    }
-}
-void remove_alarm(alarm *alarm_to_remove){
-    alarm_val *temp = alarms;
-
-    if(temp->head == NULL || alarm_to_remove == NULL){
-        puts("Error, nothing to remove");
-        return;
-    }
-
-    if(temp->head == alarm_to_remove){
-        temp->head = alarm_to_remove
-    }
-}
